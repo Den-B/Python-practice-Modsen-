9a43a6fa10ff2fb76a97fa0ef1761bd3da732a4e
Балюцкий Денис <den4kbalutski@mail.ru>
2023-05-20 13:39:47 +0300

    Добавил скрипт для документации двух эндпоинтов в формате openApi

--- source/server/index.py
+++ source/server/index.py
@@ -2,8 +2,13 @@ from flask import Flask
 from source.server.routes.delete import deleteRoute
 from source.server.routes.search import searchRoute
 
-def runServer(port):
+def createServer():
     server = Flask(__name__, static_folder='.', static_url_path='')
     server.register_blueprint(searchRoute)
     server.register_blueprint(deleteRoute)
+    return server
+
+def runServer(port):
+    server = createServer()
     server.run(port=port, debug=True)
+    return server
